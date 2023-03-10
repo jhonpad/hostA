bb124708ac7ecc6bb57321d5c00f2989360799c6
Jhon Padilla <jpadilla@paxer.com>
2023-03-10 11:28:18 -0500

    show autenticated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import { Session } from 'next-auth'
 import { useSession, signIn, signOut, getSession} from 'next-auth/react'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from './api/auth/[...nextauth]'
+
 type Props = {
   session: Session | null
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async(context: GetServerSidePropsContext) => {
-  const session = await getSession(context)
+export const getServerSideProps: GetServerSideProps<Props> = async({req, res}) => {
+  // const session = await getSession(context)
 
-  console.log('context -> ', context)
-  
+  const session = await getServerSession(req, res, authOptions)
+
+  console.log('Session -> ', session)
   return {
     props: {
       session
@@ -21,10 +25,10 @@ export const Home = ({
   session
 }: InferGetServerSidePropsType<typeof getServerSideProps>) =>  {
   console.log('env -> ', process.env.NEXTAUTH_CLIENT_ID)
+  // const { data: session } = useSession()
   const { status } = useSession()
 
-
-  console.log('Session -> ',session)
+  console.log('Session ',session)
   console.log('Status -> ',status)
 
   if (session) {
